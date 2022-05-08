42780c4400e4f404cbf66dcc7a91a75d543887b5
TagCopperLight <tagcopperlight@gmail.com>
2022-05-08 15:46:53 +0200

    mouvement update less glitched but still ...

--- server/container/entity/human/server_player.py
+++ server/container/entity/human/server_player.py
@@ -19,8 +19,8 @@ class ServerPlayer:
         self.clock = clock.Clock()
         self.__server_tps = server_tps
 
-    def get_mouvement_with_timestep(self, timestep):
-        return self.player.velocity * timestep / (1_000_000_000 / self.__server_tps)
+    def get_mouvement_with_timestep(self, timestep, velocity):
+        return velocity * timestep / (1_000_000_000 / self.__server_tps)
 
     def update_player_action(self, data):
         em_packets = []
@@ -30,24 +30,24 @@ class ServerPlayer:
             if action['action'] == key_action.ACTION_DOWN:
                 if action['key'] == key_action.KEY_RIGHT:
                     self.player.acceleration += Vector2(1, 0)
-                    self.player.position += self.get_mouvement_with_timestep(self.clock.get_time() - data['timestamp'])
+                    self.player.position += self.get_mouvement_with_timestep(self.clock.get_time() - data['timestamp'], self.player.velocity)
                 elif action['key'] == key_action.KEY_LEFT:
                     self.player.acceleration += Vector2(-1, 0)
-                    self.player.position += self.get_mouvement_with_timestep(self.clock.get_time() - data['timestamp'])
+                    self.player.position += self.get_mouvement_with_timestep(self.clock.get_time() - data['timestamp'], self.player.velocity)
                 elif action['key'] == key_action.KEY_JUMP:
                     pass
                     # self.player.acceleration += Vector2(0, 0.5)
             elif action['action'] == key_action.ACTION_UP:
                 if action['key'] == key_action.KEY_RIGHT:
                     self.player.acceleration += Vector2(-1, 0)
-                    self.player.position -= self.get_mouvement_with_timestep(self.clock.get_time() - data['timestamp'])
+                    self.player.position += self.get_mouvement_with_timestep(self.clock.get_time() - data['timestamp'], Vector2(-1, 0))
 
                     em_action = entity_move_action.EntityMoveAction(self.player)
                     em_action.timestamp = data["timestamp"]
                     em_packets.append(action_transfert_packet.ActionTransfertPacket(em_action, True).serialize())
                 elif action['key'] == key_action.KEY_LEFT:
                     self.player.acceleration += Vector2(1, 0)
-                    self.player.position -= self.get_mouvement_with_timestep(self.clock.get_time() - data['timestamp'])
+                    self.player.position += self.get_mouvement_with_timestep(self.clock.get_time() - data['timestamp'], Vector2(1, 0))
 
                     em_action = entity_move_action.EntityMoveAction(self.player)
                     em_action.timestamp = data["timestamp"]
@@ -58,6 +58,6 @@ class ServerPlayer:
     def update_player(self, world_size):
         self.player.velocity += self.player.acceleration
         self.player.acceleration = Vector2(0, 0)
-        self.player.position += self.get_mouvement_with_timestep(self.clock.time_step())
+        self.player.position += self.get_mouvement_with_timestep(self.clock.time_step(), self.player.velocity)
 
         self.player.position.x %= world_size
